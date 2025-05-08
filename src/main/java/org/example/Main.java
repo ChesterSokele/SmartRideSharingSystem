68dc3982e91cc5105d26ef3b5eb1fce43d52f31f
David Nepando <bigboy777B@proton.me>
2025-05-08 10:52:24 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            // Initialize controllers first
+            // Initializing controllers first
             MainController.rideController = new RideController();
             MainController.userController = new UserController();
             MainController.bookingController = new BookingController();
