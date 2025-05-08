51bbb7d2698fa1a74dc9a184d6fa53e1d441bbd3
David Nepando <bigboy777B@proton.me>
2025-05-08 06:06:06 +0200

    Delete src/main/java directory

--- src/main/java/controller/MainController.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-public class MainController {
-    public static RideController rideController = new RideController();
-    public static UserController userController = new UserController();
-    public static BookingController bookingController = new BookingController();
-
-}
-
